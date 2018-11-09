100e8b3e2206a215898c0a8e615e8c9fbe87ad13
Mandy <Mandy@Khronos>
2018-11-09 00:03:10 -0600

    Adding recursive method.

--- src/com/inf/searchalgorithms/BinarySearch.java
+++ src/com/inf/searchalgorithms/BinarySearch.java
@@ -1,19 +1,18 @@
 package com.inf.searchalgorithms;
-
 import java.util.Arrays;
 
 public class BinarySearch {
-
+    //Iterative implementation
 	public int runBinarySearchIteratively(
-			  int[] sortedArray, int desiredValue, int lowerIndex, int higherIndex) {
+			  int[] sortedArray, int desiredValue, int lowerIndexerIndex, int higherIndexerIndex) {
 			    int index = Integer.MAX_VALUE;
 			     
-			    while (lowerIndex <= higherIndex) {
-			        int mid = (lowerIndex + higherIndex) / 2;
+			    while (lowerIndexerIndex <= higherIndexerIndex) {
+			        int mid = (lowerIndexerIndex + higherIndexerIndex) / 2;
 			        if (sortedArray[mid] < desiredValue) {
-			            lowerIndex = mid + 1;
+			            lowerIndexerIndex = mid + 1;
 			        } else if (sortedArray[mid] > desiredValue) {
-			            higherIndex = mid - 1;
+			            higherIndexerIndex = mid - 1;
 			        } else if (sortedArray[mid] == desiredValue) {
 			            index = mid;
 			            break;
@@ -22,13 +21,37 @@ public class BinarySearch {
 			    return index;
 			}
 	
+	//Recursive implementation
+	public int binarySearchRecursively(
+			int[] sortedArray, int desiredValue, int lowerIndex, int higherIndex) {
+			    int middle = (lowerIndex + higherIndex) / 2;
+			         
+			    if (higherIndex < lowerIndex) {
+			        return -1;
+			    }
+			    if (desiredValue == sortedArray[middle]) {
+			        return middle;
+			    } else if (desiredValue < sortedArray[middle]) {
+			        return binarySearchRecursively(sortedArray, desiredValue, lowerIndex, middle - 1);
+			    } else {
+			        return binarySearchRecursively(sortedArray, desiredValue, middle + 1, higherIndex);
+			    }
+			}
+	
 	public static void main(String[] args) {
 		// Executing binary search.
 		int [] myArray = {45,12,85,32,89,39,69,44,42,1,6,8};
+		
+		//Quick sort for arrays.
 		Arrays.sort(myArray);
+		
+		//Implemented methods
 		BinarySearch bsearch = new BinarySearch();
 		int index = bsearch.runBinarySearchIteratively(myArray, 32, 0, 12);
 		System.out.println(" Value found : " + myArray[index]+ " in index: " + index);
+		int indexR = bsearch.binarySearchRecursively(myArray, 32, 0, 12);
+		System.out.println(" Value found : " + myArray[indexR]+ " in index: " + indexR);
+		
 	}
 
 }
